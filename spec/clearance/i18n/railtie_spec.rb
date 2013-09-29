57bad6f5f5d0c72325e1e11b9767972bad9014f0
Julien Itard <julienitard@gmail.com>
2013-09-29 21:39:33 +0200

    Add french locale

--- spec/clearance/i18n/railtie_spec.rb
+++ spec/clearance/i18n/railtie_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Clearance::I18n::Railtie do
   it 'adds locales to the i18n path' do
     reset_password = I18n.t('helpers.submit.password.submit', locale: :fr)
-    expect(reset_password).to eq('Reset password (Translate me!)')
+    expect(reset_password).to eq('Réinitialiser votre mot de passe')
   end
 end
